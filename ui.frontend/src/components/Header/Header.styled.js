d29c687ba1a6b2ede5bb1c6950b2c25ef08123a4
Victor Hugo <victor.vivtorhugo@gmail.com>
2022-07-28 14:40:05 -0300

    Header component working

---
+++ ui.frontend/src/components/Header/Header.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ErrorText = styled.p`
+  @import url("https://fonts.googleapis.com/css2?family=Inconsolata:wght@700&display=swap");
+  font-family: "Inconsolata", monospace;
+  font-weight: 700;
+  font-size: 1.25rem;
+`;
